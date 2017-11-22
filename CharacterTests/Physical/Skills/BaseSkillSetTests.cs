4caec437a2fc7a573be8992070f1d5bc00343b35
Paige Watson <Paige.Watson@Outlook.com>
2017-11-22 13:44:09 -0800

    Added tests for FakeWizard to BaseSkillSet

--- CharacterTests/Physical/Skills/BaseSkillSetTests.cs
+++ CharacterTests/Physical/Skills/BaseSkillSetTests.cs
@@ -28,12 +28,28 @@ namespace CharacterTests.Physical.Skills
         public void ShouldReturnSkillswithCorrectValues()
         {
             //arrange
-            FakeAttributeSet fakeAttributeSet = new FakeAttributeSet();
+            IAttributeSet fakeAttributeSet = new FakeWizardAttributeSet();
             BaseSkillSet baseSkillSet = new BaseSkillSet(fakeAttributeSet);
             //act
             List<ISkill> skillList = baseSkillSet.Skills();
             //assert
-            skillList[0].SkillBonus().Should().Be(new AttributeScore(0));
+            skillList[0].SkillBonus().Should().Be(new AttributeScore(-1));
+            skillList[1].SkillBonus().Should().Be(new AttributeScore(-1));
+            skillList[3].SkillBonus().Should().Be(new AttributeScore(0));
+            skillList[4].SkillBonus().Should().Be(new AttributeScore(0));
+            skillList[5].SkillBonus().Should().Be(new AttributeScore(0));
+            skillList[6].SkillBonus().Should().Be(new AttributeScore(0));
+            skillList[7].SkillBonus().Should().Be(new AttributeScore(0));
+            skillList[8].SkillBonus().Should().Be(new AttributeScore(0));
+            skillList[9].SkillBonus().Should().Be(new AttributeScore(0));
+            skillList[10].SkillBonus().Should().Be(new AttributeScore(0));
+            skillList[11].SkillBonus().Should().Be(new AttributeScore(0));
+            skillList[12].SkillBonus().Should().Be(new AttributeScore(0));
+            skillList[13].SkillBonus().Should().Be(new AttributeScore(2));
+            skillList[14].SkillBonus().Should().Be(new AttributeScore(2));
+            skillList[15].SkillBonus().Should().Be(new AttributeScore(2));
+            skillList[16].SkillBonus().Should().Be(new AttributeScore(2));
+            skillList[17].SkillBonus().Should().Be(new AttributeScore(2));
         }
 
     }
