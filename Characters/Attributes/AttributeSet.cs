fd747ec8f2e5c0340daa66904d60c3a830b7c1e7
Paige Watson <Paige.Watson@Outlook.com>
2017-12-20 08:32:41 -0800

    added default constructor that takes IAttributeScore objects

--- Characters/Attributes/AttributeSet.cs
+++ Characters/Attributes/AttributeSet.cs
@@ -11,16 +11,23 @@ namespace Characters.Attributes
         {
         }
 
-        public AttributeSet(int strength , int dexterity, int intellegence, int constitution, int wisdom, int charisma)
+        public AttributeSet(int strength, int dexterity, int constitution, int intellegence, int wisdom, int charisma) :
+            this(new AttributeScore(strength), new AttributeScore(dexterity), new AttributeScore(constitution),
+                new AttributeScore(intellegence), new AttributeScore(wisdom), new AttributeScore(charisma))
         {
-            _attributes.Add(new StrengthAttribute(new AttributeScore(strength)));
-            _attributes.Add(new DexterityAttribute(new AttributeScore(dexterity)));
-            _attributes.Add(new ConstitutionAttribute(new AttributeScore(intellegence)));
-            _attributes.Add(new IntellegenceAttribute(new AttributeScore(constitution)));
-            _attributes.Add(new WisdomAttribute(new AttributeScore(wisdom)));
-            _attributes.Add(new CharismaAttribute(new AttributeScore(charisma)));
+        }
+
+        public AttributeSet(IAttributeScore strScore, IAttributeScore dexScore, IAttributeScore consScore, IAttributeScore intScore, IAttributeScore wisScore, IAttributeScore chrScore)
+        {
+            _attributes.Add(new StrengthAttribute(strScore));
+            _attributes.Add(new DexterityAttribute(dexScore));
+            _attributes.Add(new ConstitutionAttribute(consScore));
+            _attributes.Add(new IntellegenceAttribute(intScore));
+            _attributes.Add(new WisdomAttribute(wisScore));
+            _attributes.Add(new CharismaAttribute(chrScore));
 
         }
+
         private readonly List<ICharacterAttribute> _attributes = new List<ICharacterAttribute>();
         public ICharacterAttribute MatchesName(IAttributeName attributeName) => GetAttributeFromName(attributeName);
         public ITextObj DisplayText()
