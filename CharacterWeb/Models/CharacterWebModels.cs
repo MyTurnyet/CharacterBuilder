75c40df0ea90c9e7edc7b4c8a0958b85c14bcdd6
Paige Watson <Paige.Watson@Outlook.com>
2018-01-09 09:20:03 -0800

    Cleaning up code, removing unused methods.

--- CharacterWeb/Models/CharacterWebModels.cs
+++ CharacterWeb/Models/CharacterWebModels.cs
@@ -5,7 +5,7 @@
     using Newtonsoft.Json;
     using J = Newtonsoft.Json.JsonPropertyAttribute;
 
-    public partial class CharacterWebModels
+    public class CharacterWebModels
     {
         [J("characterSheet")] public CharacterSheet CharacterSheet { get; set; }
     }
@@ -23,10 +23,6 @@
         [J("value")] public long Value { get; set; }
     }
 
-    public partial class CharacterWebModels
-    {
-        public static CharacterWebModels FromJson(string json) => JsonConvert.DeserializeObject<CharacterWebModels>(json, Converter.Settings);
-    }
 
     public class DeserializeWebModel
     {
@@ -37,31 +33,11 @@
             _json = json;
         }
 
-        public CharacterWebModels CharacterModels() => JsonConvert.DeserializeObject<CharacterWebModels>(_json, Converter.Settings);
-    }
-
-    public class Serialize
-    {
-        private readonly CharacterWebModels _model;
-
-        private readonly JsonSerializerSettings settings = new JsonSerializerSettings
-        {
-            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
-            DateParseHandling = DateParseHandling.None,
-        };
-        public Serialize(CharacterWebModels model)
-        {
-            _model = model;
-        }
-        public string Json() => JsonConvert.SerializeObject(_model, settings);
-    }
-
-    internal static class Converter
-    {
-        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
-        {
-            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
-            DateParseHandling = DateParseHandling.None,
-        };
+        public CharacterWebModels CharacterModels() => JsonConvert.DeserializeObject<CharacterWebModels>(_json, 
+            new JsonSerializerSettings
+            {
+                MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+                DateParseHandling = DateParseHandling.None,
+            });
     }
 }
