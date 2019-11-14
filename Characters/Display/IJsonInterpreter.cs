c78e650f402ce217f29316d54ad0e2ce310814b6
Paige Watson <paige.watson@outlook.com>
2019-11-14 11:55:10 -0800

    Add JsonInterpreter and JsonWrapper

---
+++ Characters/Display/IJsonInterpreter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Characters.Attributes;
+
+namespace Characters.Display
+{
+    public interface IJsonInterpreter
+    {
+        string Serialize(object objectToSerialize);
+    }
+}
